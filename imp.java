b3e9616a26d8ffac9eb106fccab0b72affafc9cd
kalathiya vraj <84270801+kalathiyavraj@users.noreply.github.com>
2024-04-26 11:29:01 +0530

    imp.java

--- imp.java
+++ imp.java
@@ -1,1 +1,18 @@
 https://github.com/sahildoshi013/Image-Comparison-Slider
+
+
+//image view to uri
+    public Uri getImageUriFromImageView(ImageView imageView) {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        // Save the bitmap to a file
+        String path = MediaStore.Images.Media.insertImage(
+                getContentResolver(),
+                bitmap,
+                "ImageFromImageView",
+                null
+        );
+
+        return Uri.parse(path);
+    }
